108c095d2b2b5d1f2c5eaf92feb963f96d8251f5
lucas-kitzmueller <mail@lucaskitzmueller.eu>
2020-01-12 14:48:16 -0500

    add cleaning code for base demographic model

--- clean_code/01_load_data.R
+++ clean_code/01_load_data.R
@@ -2,7 +2,7 @@ cat("\014")
 # code4policy
 
 # CHANGE DIRECTORY HERE
-setwd("/Users/lucaskitzmueller/Dropbox/code4policy_data/")
+setwd("/Users/lucaskitzmueller/Dropbox/code4policy_data/raw_data")
 # As we are not allowed to make the data public, we keep the data
 # on a private Dropbox
 
