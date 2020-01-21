e8ca47f541edea56fe6cc67ae04bf9b702601476
aragab87 <ragab.ahmed@gmail.com>
2020-01-21 09:30:52 -0500

    Converted input to dummy format

--- d3_homeworks/ahmed/example8/convert.py
+++ d3_homeworks/ahmed/example8/convert.py
@@ -1,17 +1,33 @@
-#!/usr/bin/env python3
- 
+# Additional D3 homework
+
 # load input.csv into a variable `rows`
+import csv
+import datetime
 
-# parse `modeldate` in each row and sort the `rows` based on `modeldate`
+with open('input.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+    for row in rows:
+        row['modeldate'] = datetime.datetime.strptime(row['modeldate'], "%m/%d/%Y")
 
-# loop through the rows and output a csv, `output.csv` with the date and 6 different series:
 
-# row['approve_estimate'],
-# row['disapprove_estimate'],
-# row['approve_hi'],
-# row['approve_lo'],
-# row['disapprove_hi'],
-# row['disapprove_lo']
+# Sorting by datec 
+rows = sorted(rows, key=lambda x: x['modeldate'])
 
-# don't forget the csv header. check dummy.csv
 
+# loop through the rows and output a csv with the date and 6 different series:
+
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['year','variableA','variableB','variableC','variableD', 'variableE', 'variableF'])
+
+    for row in rows:
+        writer.writerow([
+            row['modeldate'].strftime("%Y-%m-%d"),
+            row['approve_estimate'],
+            row['disapprove_estimate'],
+            row['approve_hi'],
+            row['approve_lo'],
+            row['disapprove_hi'],
+            row['disapprove_lo']
+        ])
